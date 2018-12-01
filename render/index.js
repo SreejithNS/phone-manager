7d235082b0814f572d0dca7a0c731edf64935c21
SreejithNS <nssreejith229@gmail.com>
2018-12-01 21:36:54 +0530

    Resources changed
    All resources including bootstrap and jquery are added with a little tweak in the scrip tag
    There is an unused Izimodal library.
    Removed require statement of jqeury in index and instead a script path in the index.html
    Removed require for jquery-circle and instead added directly into the html script tag
    Added window-all-closed event listener in the main process file - app.js
    Box center and modal-box class added in css and custom modal for file transfer created with working javascript code.
    Some sort of right code for drop events has been added and modified at Movies button in index.html
    app.device property added for device id and removed device list.
    Device add/remove events assignment have been seperated by Watchman function in app object
    Added wav to the known junk list
    Some changes to the clean adb shell command to work right way, Videos folder to video
    the thread variable is deleted and thus the thread is created each time for each file to be uploaded

--- render/index.js
+++ render/index.js
@@ -1,27 +1,27 @@
 var adb = require('adbkit')
-var $ = require('jquery')
 var Promise = require('bluebird');
 var client = adb.createClient()
 var path = require('path');
-require('jquery-circle-progress');
+
+
 var app = {
-    deviceList: new Map(),
-    init: (callback) => {
+    status:0,
+    device: {},
+    watchman:()=>{
         client.trackDevices()
             .then(function(tracker) {
                 tracker.on('add', function(device) {
-                    if (!app.deviceList.has(device.id)) {
-                        app.deviceList.set(device.id, device);
-                        console.log('Added new device : ' + device.id);
+                    if(!app.device.wait){
+                        app.device.id = device.id;
                         app.openFolder('/sdcard')
-                    } else {
-                        console.log('Already device exist: ' + device.id);
+                        callback();
                     }
-                    callback();
                 })
                 tracker.on('remove', function(device) {
-                    app.deviceList.delete(device.id);
+                    if(!app.device.wait){
                     console.log('Device ' + device.id + ' has been removed.');
+                    if(app.device.type !== "wifi") device.id = Error('No devices online');
+                    }
                 })
                 tracker.on('end', function() {
                     console.log('Tracking stopped');
@@ -30,6 +30,9 @@ var app = {
             .catch(function(err) {
                 console.error('Something went wrong:', err.stack)
             })
+    }
+    init: (callback) => {
+            app.watchman();
             document.addEventListener('dragover', function (event) {
               event.preventDefault();
               return false;
@@ -42,7 +45,7 @@ var app = {
     },
     cleaner:{
     	junks:{
-    		knownExt:['jpeg','jpg','mp3','mp4','m4a','aac','png'],
+    		knownExt:['jpeg','jpg','mp3','mp4','m4a','aac','png','wav'],
     		known:[],
     		unknown:[]
     	},
@@ -50,12 +53,17 @@ var app = {
             app.cleaner.junks.known.forEach((file)=>{
                 var ext = file.substr(file.lastIndexOf('.') + 1);
                 switch(true){
-                    case (ext=="mp3" || ext=="m4a"):
-                    client.shell(app.deviceList.keys().next().value, `mv "/sdcard/${file}" /sdcard/mp3`)
+                    case (ext=="mp3" || ext=="m4a" || ext=="aac" || ext=="wav"):
+                    app.shell( `mv "/sdcard/${file}" "/sdcard/Music/"`)
                     console.log(`Cleaned :${file}`)
                     break;
                     case (ext=="png" || ext=="jpeg" || ext=="jpg"):
-                    client.shell(app.deviceList.keys().next().value, `mv "/sdcard/${file}" /sdcard/DCIM/Pictures`)
+                    app.shell(`mv "/sdcard/${file}" "/sdcard/DCIM/Pictures/"`)
+                    console.log(`Cleaned :${file}`)
+                    break;
+                    case (ext=="mp4"):
+                    console.log(`mv "/sdcard/${file}" "/sdcard/Videos/"`)
+                    app.shell(`mv "/sdcard/${file}" "/sdcard/Video/"`)
                     console.log(`Cleaned :${file}`)
                     break;
                 }
@@ -65,8 +73,8 @@ var app = {
     },
     openFolder: (name,callback) => {
         var list = [];
-        app.deviceList.forEach((device) => {
-            client.readdir(device.id, name)
+        
+            client.readdir(app.device.id, name)
                 .then(function(files) {
                     files.forEach(function(file) {
                         list.push(file.name)
@@ -83,11 +91,10 @@ var app = {
                         } 
                     })
                 }).then(()=> {if(callback) callback(list)})
-        });
+        
     },
     listsdFolders:(callback)=>{
     	var list=[];
-    	app.deviceList.forEach((device) => {
             client.readdir(device.id, '/sdcard')
                 .then(function(files) {
                     files.forEach(function(file) {
@@ -99,10 +106,9 @@ var app = {
                 .then(()=>{
                     callback(list)
                 });
-        });
     },
     shell:(command)=>{
-        return client.shell(app.deviceList.keys().next().value, command)
+        return client.shell(app.device.id, command)
         .then(adb.util.readAll)
         .then(function(output) {
           console.log('%s',  output.toString().trim())
@@ -118,34 +124,20 @@ var app = {
             console.log(list)
                 list.sort().forEach((item)=>{
                     var ext = (item.indexOf(".") > -1 && item.indexOf(".") !== 0)? item.substr(item.lastIndexOf('.') + 1):'Folder';
-                    if(app.render.currentFolder == '/sdcard'){
-                        switch(item){
-                            case "DCIM":
-                            $('.container').prepend(`
-                                <div class="card" onclick="app.render.folder(app.render.currentFolder+'/${item}')" style="display:inline-block">
-                                  <div class="card-body">
-                                    <h5 class="card-title"><i class="material-icons">image</i>DCIM</h5>
-                                    <h6 class="card-subtitle mb-2 text-muted">Pictures</h6>
-                                    <p class="card-text">Contains all pictures.</p>
-                                  </div>
-                                </div>
-                                `)
-                            break;
-                            default:
-                            break;
-                        }   
-                    }else{
-                        $('#folders').append(`<tr><td onclick="app.render.folder(app.render.currentFolder+'/${item}')">${item}</td><td>${ext}</td></tr>`)
-                    }
+                    $('#folders').append(`<tr><td onclick="app.render.folder(app.render.currentFolder+'/${item}')">${item}</td><td>${ext}</td></tr>`)
                 })
             })
         },
+        Upload:(files)=>{
+            
+        }
         recieveDrop:(e)=>{
             e.preventDefault();
             var files = e.originalEvent.dataTransfer.files;
-            var thread = new Worker('uploader.js');
             $('#circle').circleProgress({startAngle: -Math.PI / 2,value: 0,lineCap: 'round',fill: {color: '#ffa500'}});
+            $("#Transfer").modal('show')
             for (let f of files) {
+                var thread = new Worker('uploader.js');
                 thread.postMessage(f.path);
                 thread.onmessage = (e)=>{
                     if(typeof e.data == "string"){
@@ -153,12 +145,14 @@ var app = {
                         .on('circle-animation-progress', function(event, progress, stepValue) {
                         $(this).find('strong').text(Math.round(100));
                         });
-                    }else{
-                    $('#circle').circleProgress('value',e.data*0.01)
-                    .on('circle-animation-progress', function(event, progress, stepValue) {
-                        $(this).find('strong').text(Math.round(e.data));
-                    });
-                }   
+                        $("#Transfer").modal('hide')
+                        delete thread;
+                        }else{
+                        $('#circle').circleProgress('value',(e.data*0.01))
+                        .on('circle-animation-progress', function(event, progress, stepValue) {
+                            $(this).find('strong').text((Math.round(e.data)==100)? 99:Math.round(e.data));
+                        });
+                        }   
                 }
             }
             return false;
@@ -167,9 +161,25 @@ var app = {
 };
 
 app.init(()=>{
+
     //app.render.folder('/sdcard');
     console.log('app.init()')
-    $('#video').on('drop',(e)=>{app.render.recieveDrop(e)});
-    $('#video').on('dragover',(e)=>$(e.target).addClass('hvr-underline-from-center'));
-    $('#video').on('dragleave dragend',(e)=>{$(e.target).removeClass('hvr-underline-from-center');console.log("over")});
+    //$('#video').on('drop',(e)=>{app.render.recieveDrop(e)});
+    //$('#video').on('dragover',(e)=>$(e.target).addClass('hvr-underline-from-center'));
+    //$('#video').on('dragleave dragend',(e)=>{$(e.target).removeClass('hvr-underline-from-center');console.log("over")});
+
+  $('#video').on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  })
+  .on('dragover dragenter', function(e) {
+    console.log(e.originalEvent.dataTransfer.files)
+    $('#video').addClass('hvr-underline-from-center');
+  })
+  .on('dragleave dragend drop', function() {
+    $('#video').removeClass('hvr-underline-from-center');
+  })
+  .on('drop', function(e) {
+    app.render.recieveDrop(e)
+  });
 });
